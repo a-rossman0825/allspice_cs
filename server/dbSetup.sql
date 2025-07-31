3f846336a0bf92c840cc1b06b419d9e718166e3e
Anthony Rossman <anthony.rossman0825@gmail.com>
2025-07-31 14:08:02 -0600

    dbSetup recipes table

--- server/dbSetup.sql
+++ server/dbSetup.sql
@@ -6,3 +6,25 @@ CREATE TABLE IF NOT EXISTS accounts(
   email VARCHAR(255) UNIQUE COMMENT 'User Email',
   picture VARCHAR(255) COMMENT 'User Picture'
 ) default charset utf8mb4 COMMENT '';
+
+
+CREATE TABLE recipes(
+  id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+  created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
+  updated_at DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  title VARCHAR(255) NOT NULL,
+  instructions VARCHAR(5000),
+  img VARCHAR(1000) NOT NULL,
+  category ENUM('breakfast', 'lunch', 'dinner', 'snack', 'dessert'),
+  creator_id VARCHAR(255) NOT NULL,
+  FOREIGN KEY (creator_id) REFERENCES accounts(id) ON DELETE CASCADE
+  ) default charset utf8mb4;
+
+  SELECT
+  recipes.*,
+  accounts.*
+  FROM recipes
+  JOIN accounts ON recipes.creator_id = accounts.id
+  WHERE recipes.id = 1;
+
+  
